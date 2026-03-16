32f8e8ac222b759ded23239e599699a413a2a37f
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-03-16 23:50:09 +0300

    refactor: refactor js code for box-model example

--- content/ljtech/collections/web-fundamentals/posts/web-fundamentals-html/box-model/src/script.js
+++ content/ljtech/collections/web-fundamentals/posts/web-fundamentals-html/box-model/src/script.js
@@ -1,32 +1,11 @@
 document.getElementById("styleBtn").addEventListener("click", function () {
-	const boxes = [];
-
 	const marginBox = document.getElementById("marginBox");
 	const borderBox = document.getElementById("borderBox");
 	const paddingBox = document.getElementById("paddingBox");
 	const contentBox = document.getElementById("contentBox");
 
-	if (marginBox.classList.contains("box-margin-styled")) {
-		marginBox.classList.remove("box-margin-styled");
-	} else {
-		marginBox.classList.add("box-margin-styled");
-	}
-
-	if (borderBox.classList.contains("box-border-styled")) {
-		borderBox.classList.remove("box-border-styled");
-	} else {
-		borderBox.classList.add("box-border-styled");
-	}
-
-	if (paddingBox.classList.contains("box-padding-styled")) {
-		paddingBox.classList.remove("box-padding-styled");
-	} else {
-		paddingBox.classList.add("box-padding-styled");
-	}
-
-	if (contentBox.classList.contains("box-content-styled")) {
-		contentBox.classList.remove("box-content-styled");
-	} else {
-		contentBox.classList.add("box-content-styled");
-	}
+	marginBox.classList.toggle("box-margin-styled");
+	borderBox.classList.toggle("box-border-styled");
+	paddingBox.classList.toggle("box-padding-styled");
+	contentBox.classList.toggle("box-content-styled");
 });
